743ba35003588da1930d0b5f6007d240df88fd68
Teodor Kostadinov <teodor.kk.93@gmail.com>
2014-07-19 01:26:05 +0300

    Initial project created

---
+++ gen/com/vratsasoftware/codeweekschedule/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.vratsasoftware.codeweekschedule;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
